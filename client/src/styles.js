fcfc0cf6cc751573d727886fe7e1eb4e15692615
Hamse Hassan <hamse>
2023-11-12 17:00:02 +0300

    first commit

--- client/src/styles.js
+++ client/src/styles.js
@@ -2,8 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   appBar: {
-    borderRadius: 15,
-    margin: '30px 0',
+    borderRadius: 29,
+    margin: '15px 0',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
